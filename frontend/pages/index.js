70d9fac7d27a560e3d9e097fb9aa2ea2505d3998
BenBktech <bbrucher@gmail.com>
2023-01-24 10:30:45 +0100

    Refactoring...

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
-import Link from 'next/link'
 import { Layout } from "../components/Layout/Layout"
-import { Heading, Flex, Text, Textarea, Input, Button, useToast, Card, CardHeader, CardBody, CardFooter, Alert, AlertIcon } from '@chakra-ui/react'
+import { Heading, Flex, Text, Textarea, Input, Button, useToast, Alert, AlertIcon } from '@chakra-ui/react'
 import { useState, useEffect } from 'react'
 import { useAccount, useProvider, useSigner } from 'wagmi'
 import Contract from "../../backend/artifacts/contracts/Job.sol/Jobs.json"
 import { ethers } from 'ethers'
+import { Job } from '../components/Job/Job'
 
 export default function Home() {
 
@@ -40,21 +40,19 @@ export default function Home() {
 
     let events = await contract.queryFilter(filter)
     let allTheEvents = [], jobAddedEvents = [], jobTakenEvents = [], jobPaidEvents = [];
-    //First, we need to get all the events, they are 3 types of events : jobAdded, jobTaken and jobFinished, so let's sort them first.
-    for await (const event of events) {
-        const txnReceipt = await event.getTransactionReceipt()
-        let eventLog = txnReceipt.logs[0] 
-        let log = contract.interface.parseLog(eventLog)
-        if(log.eventFragment.name === "jobAdded") {
-          jobAddedEvents.push(log.args)
-        }
-        else if(log.eventFragment.name === "jobTaken") {
-          jobTakenEvents.push(log.args)
-        }
-        else {
-          jobPaidEvents.push(log.args)
-        }
-    }
+
+    //For each event, we put it in the right array
+    events.forEach(event => {
+      if(event.event === "jobAdded") {
+        jobAddedEvents.push(event.args)
+      }
+      else if(event.event === "jobTaken") {
+        jobTakenEvents.push(event.args)
+      }
+      else {
+        jobPaidEvents.push(event.args)
+      }
+    })
 
     //Then we need to iterate through the jobAdded events and if a job is taken or finished, we put these infos in a new object for this job
     let jobs = []
@@ -82,10 +80,10 @@ export default function Home() {
       })
       jobs.push(thisJob)
     })
-
     setEvents(jobs)
   }
 
+  //The user wants to take a job
   const takeJob = async(id) => {
     try {
       const contract = new ethers.Contract(contractAddress, Contract.abi, signer)
@@ -109,9 +107,9 @@ export default function Home() {
         isClosable: true,
       })
     }
-    
   }
 
+  //The user wants to pay a job
   const payJob = async(id) => {
     try {
       const contract = new ethers.Contract(contractAddress, Contract.abi, signer)
@@ -150,40 +148,12 @@ export default function Home() {
           width="100%" 
           direction={["column", "column", "row", "row"]} 
           alignItems={["center", "center", "flex-start", "flex-start"]}
-          justifyContent={[]}
           flexWrap="wrap"
         >
           {events.length !== 0 ? (
             events.map(event => {
               return (
-                <Card key={crypto.randomUUID()} mt={["1rem", "1rem", 0,0]} minWidth={["100%", "100%", "30%", "30%"]} ml="1%" mr="1%">
-                  <CardBody>
-                    <Text><Text as="span" fontWeight="bold">Author :</Text> {event.author.substring(0, 5)}...{event.author.substring(event.author.length - 4)}</Text>
-                    <Text mt="1rem" mb="1rem"><Text as="span" fontWeight="bold">Description</Text> : {event.description}</Text>
-                    {/* if the job is finished we display that the job is finished */}
-                    {event.isFinished ? (
-                      <Text color="red" fontWeight="bold">Job is finished.</Text>
-                    ) : (
-                      /* If the job is not finished, is the job already taken? */
-                      event.isTaken ? (
-                        /* If the job is taken, a button to pay the worker is displayed.
-                        that you are the author of the job to display this button */
-                        address === event.author ? (
-                          <Button colorScheme="red" onClick={() => payJob(event.id)}>Pay</Button>
-                        ) : (
-                          /* Otherwise, it is displayed that the job is taken */
-                          <Text color="green" fontWeight="bold">Job taken.</Text>
-                        )
-                      ) : (
-                        /* if the job is not taken, a button is displayed to take the job, but it is necessary to check
-                        that the connected address is not the one of the author of the job */
-                        address !== event.author && (
-                          <Button colorScheme="whatsapp" onClick={() => takeJob(event.id)}>Work</Button> 
-                        )
-                      )
-                    )}                
-                  </CardBody>
-                </Card>
+                <Job event={event} takeJob={takeJob} payJob={payJob} />
               )
             })
           ) : (
@@ -192,7 +162,7 @@ export default function Home() {
                 <AlertIcon />
                 <Flex direction="column">
                   <Text as='span'>There are no jobs on our DApp.</Text>
-                  <Text><Link href="addajob" style={{"font-weight": "bold"}}>Create the first job!</Link></Text>
+                  <Text><Link href="addajob" style={{"fontWeight": "bold"}}>Create the first job!</Link></Text>
                 </Flex>
               </Alert>
             </Flex>
